25183d7645ff2f8fbe5f20b2cbe5103525706c68
C Vikram Simha Reddy <namagondlavinay@gmail.com>
2019-09-25 22:34:34 +0530

    modifying output line in app.java

--- Calucator/src/main/java/com/app/Calucator/App.java
+++ Calucator/src/main/java/com/app/Calucator/App.java
@@ -9,6 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println("hello samsung this is my first git line");
+        System.out.println("hello Nokia this is my first git line");
+        
     }
 }
